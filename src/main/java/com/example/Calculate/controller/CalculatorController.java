2a598be505215a285c7a2152360cb5c740f8de3e
NicatHsynlyv <nicat200507@gmail.com>
2026-03-17 21:02:33 +0800

    fix package names

--- src/main/java/com/example/Calculate/controller/CalculatorController.java
+++ src/main/java/com/example/Calculate/controller/CalculatorController.java
@@ -1,6 +1,6 @@
-package Controller;
+package com.example.Calculate.controller;
 
-import Service.CalculatorService;
+import com.example.Calculate.service.CalculatorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/example/Calculate/service/CalculatorService.java
+++ src/main/java/com/example/Calculate/service/CalculatorService.java
@@ -1,4 +1,4 @@
-package Service;
+package com.example.Calculate.service;
 
 import org.springframework.stereotype.Service;
 
